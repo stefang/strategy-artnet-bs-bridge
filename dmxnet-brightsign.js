7e7ad71768edd995c27f708826a7c0f1335f8be3
Stefan Goodchild <me@stefangoodchild.com>
2020-10-22 10:04:27 +0100

    Move to public npm repo

--- dmxnet-brightsign.js
+++ dmxnet-brightsign.js
@@ -1,19 +1,30 @@
-var dmxlib = require('@stefang/dmxnet');
-window.dmx = {};
-window.dmx.lib = new dmxlib.dmxnet({
-  verbose: 0,
-});
-window.dmx.sender = window.dmx.lib.newSender({
-  ip: "192.168.68.115",
-  port: 6454,
-  subnet: 0,
-  universe: 0,
-  net: 0,
-});
-window.dmx.send = function(msg) {
-  console.log(msg);
-  for (const [key, value] of Object.entries(msg)) {
-    window.dmx.sender.prepChannel(parseInt(key), value);
-  }
-  window.dmx.sender.transmit();
+try {
+  var dmxlib = require('@stefang/dmxnet');
+  /**
+   * Call when we know the host and port to connect to
+   * @param options {host:string, port:string}
+   */
+  window.createBrightSignArtnetBridge = options => {
+    const {host, port} = options;
+    window.dmx = {};
+    window.dmx.lib = new dmxlib.dmxnet({
+      verbose: 0,
+    });
+    window.dmx.sender = window.dmx.lib.newSender({
+      ip: host,
+      port: port,
+      subnet: 0,
+      universe: 0,
+      net: 0,
+    });
+    window.dmx.send = function(msg) {
+      console.log(msg);
+      for (const [key, value] of Object.entries(msg)) {
+        window.dmx.sender.prepChannel(parseInt(key), value);
+      }
+      window.dmx.sender.transmit();
+    };
+  };
+} catch (e) {
+  console.log('strategy-artnet-bs-bridge only runs on BrightSign');
 }
